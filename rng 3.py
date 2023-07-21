aa1db42bf8747c3d776366c0f8347539b67c18ae
turkishpen <122563709+turkishpen@users.noreply.github.com>
2023-07-21 16:15:58 +0300

    Add files via upload

---
+++ rng 3.py
@@ -0,0 +1,26 @@
+#şans çarkı
+#%10
+
+import random
+
+sayı=random.randint(0,9)
+if sayı==0:
+    print("zıpla")
+if sayı==1:
+    print("şarkı söyle")
+if sayı==2:
+    print("ağaç resmi çiz")
+if sayı==3:
+    print("çiçek resmi çiz")
+if sayı==4:
+    print("yeniden çevir")
+if sayı==5:
+    print("su iç")
+if sayı==6:
+    print("yeniden çevir")
+if sayı==7:
+    print("bir şey yapma")
+if sayı==8:
+    print("sağa dön")
+if sayı==9:
+    print("sola dön")
