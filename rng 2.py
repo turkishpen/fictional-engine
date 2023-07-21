5bd76495c2f7fbbcae98f56ff6de64ea3b5c7f33
turkishpen <122563709+turkishpen@users.noreply.github.com>
2023-07-21 16:10:02 +0300

    rng 2.py

---
+++ rng 2.py
@@ -0,0 +1,9 @@
+#milli piyango
+#1/9000000
+import random
+
+sayı=random.randint(1000000,9999999)
+if sayı==6075534:
+    print("BÜYÜK ikramiyeyi kazandın!!!!!!")
+else:
+   print("daha sonra sen de kazanırsın.  sayın:", + sayı)
